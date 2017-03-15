66e86d1cd1ea90a6806801f0b97a7d55cdf6912c
심철환 <a@akaiv.com>
2017-03-15 18:43:24 +0800

    Start to use ES6

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,65 +1,31 @@
 module.exports = function (grunt) {
   'use strict';
 
-  var bootstrapScripts = [
-    'assets/bootstrap/js/dist/util.js',
-    'assets/bootstrap/js/dist/alert.js',
-    'assets/bootstrap/js/dist/button.js',
-    'assets/bootstrap/js/dist/carousel.js',
-    'assets/bootstrap/js/dist/collapse.js',
-    'assets/bootstrap/js/dist/dropdown.js',
-    'assets/bootstrap/js/dist/modal.js',
-    'assets/bootstrap/js/dist/scrollspy.js',
-    'assets/bootstrap/js/dist/tab.js',
-    'assets/bootstrap/js/dist/tooltip.js',
-    'assets/bootstrap/js/dist/popover.js'
-  ];
-
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
     clean: {
-      js: 'js/',
-      bootstrap: '<%= copy.bootstrap.dest %>'
+      js: 'js',
+      css: '<%= copy.bootstrap.dest %>'
     },
 
     copy: {
-      jquery: {
-        expand: true,
-        cwd: 'assets/jquery/dist/',
-        src: 'jquery.min.js',
-        dest: 'js/'
-      },
       bootstrap: {
         expand: true,
-        cwd: 'assets/bootstrap/scss/',
+        cwd: 'node_modules/bootstrap/scss/',
         src: ['**/*'],
         dest: '_sass/vendors/bootstrap/'
       }
     },
 
-    jshint: {
-      options: { jshintrc: '_scripts/.jshintrc' },
-      grunt: {
-        options: { jshintrc: '.jshintrc' },
-        src: 'Gruntfile.js'
-      },
-      core: { src: '_scripts/*.js' }
+    eslint: {
+      target: ['_scripts/*.js']
     },
 
-    jscs: {
-      options: { config: 'assets/bootstrap/js/.jscsrc' },
-      grunt: { src: '<%= jshint.grunt.src %>' },
-      core: { src: '<%= jshint.core.src %>' }
-    },
-
-    concat: {
-      core: {
-        src: [
-          bootstrapScripts,
-          '_scripts/main.js'
-        ],
-        dest: 'js/main.js'
+    babel: {
+      options: { sourceMap: true },
+      dist: {
+        files: { 'js/main.js': '_scripts/main.js' }
       }
     },
 
@@ -69,29 +35,25 @@ module.exports = function (grunt) {
         mangle: true
       },
       core: {
-        src: '<%= concat.core.dest %>',
+        src: 'js/main.js',
         dest: 'js/main.min.js'
       }
     },
 
     watch: {
       js: {
-        files: '<%= jshint.core.src %>',
-        tasks: 'build',
-        options: { livereload: true }
-      },
-      grunt: {
-        files: 'Gruntfile.js',
-        tasks: 'build',
+        files: '<%= eslint.target %>',
+        tasks: 'js',
         options: { livereload: true }
       }
     }
-
   });
 
   require('load-grunt-tasks')(grunt, { scope: 'devDependencies' });
   require('time-grunt')(grunt);
 
-  grunt.registerTask('build', ['clean', 'copy', 'jshint', 'jscs', 'concat', 'uglify']);
+  grunt.registerTask('js', ['clean:js', 'eslint', 'babel', 'uglify']);
+  grunt.registerTask('css', ['clean:css', 'copy']);
+  grunt.registerTask('build', ['js', 'css']);
   grunt.registerTask('default', ['build']);
 };
--- README.md
+++ README.md
@@ -7,19 +7,18 @@
 이 프로젝트를 클론했다면 아래 파일들을 편집해서 프로젝트 설정을 해주세요:
 ```
 _config.yml
-bower.json
 package.json
 README.md
 ```
 
 ### 패키지 설치
-이 프로젝트를 활용하려면 [Bower](http://bower.io/)와 [Grunt](http://gruntjs.com/)가 필요합니다.
+이 프로젝트를 활용하려면 [Grunt](http://gruntjs.com/)가 필요합니다.
 ```
-npm install -g bower grunt-cli
+npm install -g grunt-cli
 ```
 
-Bower와 Grunt를 설치했으면 다음 명령어를 통해 필요한 패키지를 설치합니다:
+Grunt를 설치했으면 다음 명령어를 통해 필요한 패키지를 설치합니다:
 ```
 npm install
 ```
-위 명령어는 `bower install && grunt build`를 포함하고 있습니다.
+위 명령어는 `grunt build`를 포함하고 있습니다.
